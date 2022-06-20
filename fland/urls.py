237f0987f68c2a149c7f5536ccdf5ed9e98a6e01
Shohrux99 <b.shohrux@mail.ru>
2022-06-20 11:15:17 +0500

    .

--- fland/urls.py
+++ fland/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
-from .views import IndexView
+from .views import IndexView,first,second,third,fourth,fifth
 urlpatterns = [
     path('',IndexView,name='index'),
+    path('first/', first,name='first'),
+    path('second/', second,name='second'),
+    path('third/', third,name='third'),
+    path('fourth/', fourth,name='fourth'),
+    path('fifth/', fifth,name='fifth'),
 ]
--- fland/views.py
+++ fland/views.py
@@ -6,3 +6,23 @@ from django.shortcuts import render
 def IndexView(request):
 
     return render(request,'main/index.html')
+
+def first(request):
+
+    return render(request,'main/1.html')
+
+def second(request):
+
+    return render(request,'main/2.html')
+
+def third(request):
+
+    return render(request,'main/3.html')
+
+def fourth(request):
+
+    return render(request,'main/4.html')
+
+def fifth(request):
+
+    return render(request,'main/5.html')
